d6c5cceb8224c183b93cf5eb992835afa989a256
Nathan Coburn <nathanlcoburn@gmail.com>
2019-01-08 01:32:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 /* code goes here */
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector("body");
+  let hidden_div= document.querySelector("#hidden-div");
+  hidden_div.innerText= 301
+  }
+)
