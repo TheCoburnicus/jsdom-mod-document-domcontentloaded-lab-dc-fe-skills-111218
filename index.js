3a8a9d5f54ce107db635bfc16a676178b6dae668
Nathan Coburn <nathanlcoburn@gmail.com>
2019-01-08 01:31:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   document.querySelector("body");
   let hidden_div= document.querySelector("#hidden-div");
-  switch (style) {
-    case 301:
-      hidden_div = true;
-      console.log('case 301: ', hidden_div)
-    default:
-      hidden-div = false;
-      console.log('default: ', hidden_div)
+  hidden_div.innerText= 301
   }
 })
